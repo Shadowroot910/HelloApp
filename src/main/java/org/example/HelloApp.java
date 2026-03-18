6992ee196598cc2e99cff7a1c6316c50acf65024
cyberfrostX <hj5713@srmist.edu.in>
2026-03-18 09:00:13 +0530

    UC3

--- src/main/java/org/example/HelloApp.java
+++ src/main/java/org/example/HelloApp.java
@@ -1,12 +1,10 @@
 package org.example;
 
-public class cmdArg {
+public class uc3 {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
-        }
+
+        String name = (args.length > 0) ? args[0] : "Himanshi";
+
+        System.out.println("Hello, " + name + "!");
     }
 }
